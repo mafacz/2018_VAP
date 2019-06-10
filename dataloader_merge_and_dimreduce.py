a345f1dbaa53cb157b16d51eee9207fb9639899a
martin@faltys.ch <martin@faltys.ch>
2019-06-10 16:01:45 +0200

    vm failure

--- dataloader_merge_and_dimreduce.py
+++ dataloader_merge_and_dimreduce.py
@@ -4,20 +4,23 @@ import numpy as np
 
 #enviroment variables
 data_path = "/cluster/work/grlab/clinical/Inselspital/DataReleases/01-19-2017/InselSpital/"
-input_dir = "paO2_prediction"
-output_dir = "paO2_prediction"
+input_dir = "derived_mafacz/VAP"
+output_dir = "derived_mafacz/VAP"
 general_data_file = os.path.join(data_path, "1_hdf5_consent/180704", "generaldata.h5")
 mon_data_file = os.path.join(data_path, input_dir, "monvals_pivot.h5")
 der_data_file = os.path.join(data_path, input_dir, "derv_pivot.h5")
 lab_data_file = os.path.join(data_path, input_dir, "labres_pivot.h5")
 observ_data_file = os.path.join(data_path, input_dir, "observ_pivot.h5")
 output_data_file = os.path.join(data_path, output_dir, "merged_pivot.h5")
+output_data_file_5 = os.path.join(data_path, output_dir, "merged_pivot_five_percent.h5")
 
 #Read General File for Patient IDs
 df_gen = pd.read_hdf(general_data_file)
 
 #Loop trough all patients and add them to final table after concatiating all tables and joining different columns of same base concepts
+i = 0
 for patientID in df_gen.PatientID:
+	i = i + 1
 	print(patientID)
 	#concatinate
 	df_mon_pat = pd.read_hdf(mon_data_file, where='PatientID==' + str(patientID))
@@ -29,12 +32,15 @@ for patientID in df_gen.PatientID:
 	df_concat.index.names = ['DateTime', 'PatientID']
 	
 	#reduce dimensionality
-	df_concat["m20"] = df_concat[['4000', '8280']].mean(axis=1)
-	df_concat["m21"] = df_concat[['2200', '8290', '30010009']].mean(axis=1)
-	df_concat["m2"] = df_concat[['410', '7100', '400', '7120']].mean(axis=1)
+	df_concat["SpO2"] = df_concat[['4000', '8280']].mean(axis=1)
+	df_concat["etCO2"] = df_concat[['2200', '8290', '30010009']].mean(axis=1)
+	df_concat["Temp"] = df_concat[['410', '7100', '400', '7120']].mean(axis=1)
 
 	if len(df_concat.index) > 0:
 		df_concat.to_hdf(output_data_file, 'pivoted', append=True, complevel=5, complib='blosc:lz4', data_columns=['PatientID'], format='table')
+
+	if len(df_concat.index) > 0 and i % 20 == 0:
+		df_concat.to_hdf(output_data_file_5, 'pivoted', append=True, complevel=5, complib='blosc:lz4', data_columns=['PatientID'], format='table')
 
 	
 	
--- dataloader_patient_table_for_vap.py
+++ dataloader_patient_table_for_vap.py
@@ -5,14 +5,18 @@ import variable_references as var
 
 #enviroment variables
 data_path = "/cluster/work/grlab/clinical/Inselspital/DataReleases/01-19-2017/InselSpital/"
-input_dir = "paO2_prediction"
-output_dir = "paO2_prediction"
+input_dir = "derived_mafacz/VAP"
+output_dir = "derived_mafacz/VAP"
 general_data_file = os.path.join(data_path, input_dir, "generaldata.h5")
 merged_data_file = os.path.join(data_path, input_dir, "merged_pivot.h5")
 output_data_file = os.path.join(data_path, output_dir, "vap_ventilated_pivot.h5")
 
 #Read General File for Patient IDs
 df_gen = pd.read_hdf(general_data_file)
+
+var_fio2 = '2010'
+var_peep = '3200'
+var_leuk = '20000700'
 
 #Loop trough all patients and add them to final table after concatiating all tables and joining different columns of same base concepts
 for patientID in df_gen[df_gen.AdmissionTime > '01.01.2008'].PatientID:
@@ -22,13 +26,15 @@ for patientID in df_gen[df_gen.AdmissionTime > '01.01.2008'].PatientID:
 	df_merged_pat = pd.read_hdf(merged_data_file, where='PatientID==' + str(patientID))
 	
 	#derived values for filtering
-	df_merged_pat[var.MechanicalVentilation] = False | (df_merged_pat[var.etCO2] > 1) | (df_merged_pat[var.etCO2].shift(1) > 1) | (df_merged_pat[var.etCO2].shift(2) > 1) | (df_merged_pat[var.etCO2].shift(3) > 1) | (df_merged_pat[var.etCO2].shift(-1) > 1) | (df_merged_pat[var.etCO2].shift(-2) > 1) | (df_merged_pat[var.etCO2].shift(-3) > 1)  
+	#CAVE THIS HAS TO BE CHANGED
+	df_merged_pat['MechanicalVentilation'] = False | (df_merged_pat[var.etCO2] > 1) | (df_merged_pat[var.etCO2].shift(1) > 1) | (df_merged_pat[var.etCO2].shift(2) > 1) | (df_merged_pat[var.etCO2].shift(3) > 1) | (df_merged_pat[var.etCO2].shift(-1) > 1) | (df_merged_pat[var.etCO2].shift(-2) > 1) | (df_merged_pat[var.etCO2].shift(-3) > 1)  
 
-	#use only timepoints with fiO2 or peep measurements
-	df_merged_pat = df_merged_pat.loc[np.logical_not(df_merged_pat[var.fiO2].isnull() & df_merged_pat[var.peep].isnull() & df_merged_pat[var.Leuk].isnull() & df_merged_pat[var.TempCore].isnull() )]
+	#use only timepoints where one of the key variables is not null
+	df_merged_pat = df_merged_pat.loc[np.logical_not(df_merged_pat[var_fio2].isnull() & df_merged_pat[var_peep].isnull() & df_merged_pat[var_leuk].isnull() & df_merged_pat["Temp"].isnull())]
 	df_merged_pat.reset_index(inplace=True)
 
-	columns_of_interest = ['PatientID', 'DateTime', var.fiO2, var.peep, var.MechanicalVentilation, var.Leuk, var.TempCore]
+	#exporting the columns patientid, datetime, fio2, peep, mechanical ventilation, Leuk, Temp
+	columns_of_interest = ['PatientID', 'DateTime', var_fio2, var_peep, 'MechanicalVentilation', var_leuk, "Temp"]
 	df_small = df_merged_pat[columns_of_interest]
 
 	if len(df_small[df_small[var.MechanicalVentilation]]) > 0:
